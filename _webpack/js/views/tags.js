1097581c3cc94841bd57183f47e1d7f014d47b41
v20100v <7567933+v20100v@users.noreply.github.com>
2021-02-26 19:06:35 +0100

    Add features with "Tag", and a special "Tags" page

---
+++ _webpack/js/views/tags.js
@@ -0,0 +1,13 @@
+import '../../scss/views/tags.scss';
+import * as Dom from "../utils/Dom";
+
+Dom.ready(() => {
+
+    /**
+     * In tartiflette we trust !
+     */
+    (function initView() {
+        console.debug('[6i-Jekyll] Init "Tags" view');
+
+    })();
+});
